fcca3c177fdab3d82d10121d127212e7d0f00f97
aprilespiritu <april_espiritu@live.com>
2024-07-23 16:48:36 +0800

    Hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal/Modal';
 import css from './ImageGalleryItem.module.css';
---
+++ src/components/hooks/useToggle.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const useToggle = (initialState = false) => {
+    const [showModal, setShowModal] = useState(initialState);
+
+    const toggle = () => setShowModal(!showModal);
+
+    return { showModal, toggle };
+};
