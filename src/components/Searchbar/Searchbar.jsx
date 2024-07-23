f87ba74cc0898ae748f384ecf326c2d29aa3a660
aprilespiritu <april_espiritu@live.com>
2024-07-23 20:35:24 +0800

    Searchbar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import css from './Searchbar.module.css';
+import { FaSearch } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+
+const Searchbar = ({ onSubmit }) => {
+    const [search, setSearch] = useState('');
+
+    const handleChange = e => {
+        setSearch(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        onSubmit(search);
+        setSearch('');
+    };
+
+    return (
+        <header className={css.searchbar}>
+            <form className={css.form} onSubmit={handleSubmit}>
+                <button type="submit" className={css.button}>
+                    <FaSearch />
+                    <span className={css.buttonLabel}></span>
+                </button>
+
+                <input
+                    className={css.input}
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images"
+                    value={search}
+                    onChange={handleChange}
+                />
+            </form>
+        </header>
+    );
+};
+
+SearchBar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+}
+
+export default Searchbar;
